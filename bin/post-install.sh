2a35f251190ca6c9848aa689c012923adcdf1d06
RobertTheNerd <robert.q.wang@gmail.com>
2016-08-27 09:11:28 -0700

    google-chrome

--- bin/post-install.sh
+++ bin/post-install.sh
@@ -98,7 +98,7 @@ post_install() {
     # google-chrome
     wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
     sudo dpkg -i google-chrome-stable_current_amd64.deb
-    sudo apt-get install -f -y
+    sudo apt-get install -fy
     rm google-chrome-stable_current_amd64.deb
 
     # sublime text, https://gist.github.com/simonewebdesign/8507139
