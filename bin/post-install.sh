ecf7202e3197b88dd9c2d6ff5e691d5e59932136
RobertTheNerd <robert.q.wang@gmail.com>
2017-10-04 13:10:01 -0700

    Fix dotnet core script

--- bin/post-install.sh
+++ bin/post-install.sh
@@ -97,7 +97,8 @@ post_install() {
     curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
     sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
     sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-xenial-prod xenial main" > /etc/apt/sources.list.d/dotnetdev.list'
-    sudo apt-get updatesudo apt-get install dotnet-sdk-2.0.0
+    sudo apt-get update
+    sudo apt-get install dotnet-sdk-2.0.0
 
     # google-chrome
     wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
