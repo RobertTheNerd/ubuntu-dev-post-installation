740088ba73bc264c54336ce626a7d38cf3b34718
RobertTheNerd <robert.q.wang@gmail.com>
2017-10-04 13:13:11 -0700

    Surpress y/n prompt for dotnet core.

--- bin/post-install.sh
+++ bin/post-install.sh
@@ -43,7 +43,7 @@ post_install() {
     sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
     sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-xenial-prod xenial main" > /etc/apt/sources.list.d/dotnetdev.list'
     sudo apt-get update
-    sudo apt-get install dotnet-sdk-2.0.0
+    sudo apt-get install -y dotnet-sdk-2.0.0
     
     # Node.js
     curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
